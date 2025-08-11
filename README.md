b5e190afe3e3e8d3884530505aa2012c7cfa53c4
GavinAtCWU <Gavin.Spomer@cwu.edu>
2025-08-11 13:28:35 -0700

    Updated README

--- README.md
+++ README.md
@@ -4,10 +4,10 @@
 ## Overview
 
 On February 12 2024, [Ex Libris announced a Showcase widget](https://exlibrisgroup.com/announcement/announcement-new-showcase-carousel-on-ex-libris-primo/) 
-with books from your Primo instance displayed with a carousel that you can embed in a web page. While some will be savy enough to follow 
+that displays books from your Primo instance in a carousel which you can embed in a web page. While some will be savy enough to follow 
 [Ex Libris' instructions](https://developers.exlibrisgroup.com/blog/primo-showcase-how-to-embed/), others may find that a bit too technical to 
 implement. The purpose of the **showcase-generator.js** script is to not only make this easier for non-technical people to implement, but to make it a 
-bit quicker and more convenient for *everyone*. Using it to generate HTML code for the widgets is simple:
+bit quicker and more convenient for *everyone*. Using it to generate HTML code for Showcase widgets is simple:
 
 1. Search for books in your Primo instance and copy the resulting URL from your web browser.
 
@@ -28,15 +28,17 @@ and contains the Javascript code that turns the &lt;search-carousel&gt; element 
 * **style.css**: Minimal stylesheet to support **index.html** above.
 
 
-## Installing
+## Setup
 
 1. Follow the instructions for 
 [Adding Domains for Discovery Showcase](https://knowledge.exlibrisgroup.com/Primo/Product_Documentation/020Primo_VE/Primo_VE_(English)/120Other_Configurations/Adding_Domains_for_Discovery_Showcase) 
 to add domains that will have permission to embed Showcase widgets on. For example, if you want to have Discovery Showcase widgets in your LibGuides 
-and your libguides are at **https://libguides.yourschool.edu**, you will need to add that domain.
+and they are at **https://libguides.yourschool.edu**, you will need to add that domain.
 
-2. Download this project's files to the computer you want to use the Showcase generator on. It doesn't matter if this computer is a desktop, laptop or 
-server as long as its domain is setup in step 1 above.
+2. Download this project's files to the computer you want to use the Showcase widget **generator** on. It doesn't matter if this computer is a desktop, 
+laptop or server, however in order for the preview (of the Showcase widget) to work, the computer's domain needs to be setup in 
+
+3. Copy 
 
 
 ## Using
--- showcase-generator.js
+++ showcase-generator.js
@@ -1,64 +1,99 @@
 
 function generateShowcase() {
 
-	// Get Primo base URL and search parameters and initialize PNX search URL
+	// Get Primo base URL and search parameters and initialize PNX search URL with default parameters.
 	let primoSearchUrl = new URL(document.getElementById('primo_search_url').value);
-	let primoPNXSUrl = primoSearchUrl.origin + '/primaws/rest/external/pnxs?';
+	let APIDefaultParamsString = 'acTriggered=false&' +
+		'blendFacetsSeparately=false&' +
+		'came_from=sort&' +
+		'citationTrailFilterByAvailability=true&' +
+		'disableCache=false&' +
+		'getMore=0&' +
+		'isCDSearch=false&' +
+		'lang=en&' +
+		'limit=10&' +
+		'newspapersActive=true&' +
+		'newspapersSearch=false&' +
+		'offset=0&' +
+		'otbRanking=false&' +
+		'pcAvailability=true&' +
+		'rapido=true&' +
+		'refEntryActive=false&' +
+		'rtaLinks=true&' +
+		'searchInFulltextUserSelection=false&' +
+		'skipDelivery=Y&';
+	let primoAPIUrl = primoSearchUrl.origin + '/primaws/rest/external/pnxs?' + APIDefaultParamsString;
 	let searchParams = primoSearchUrl.search.split('&');
 	searchParams[0] = searchParams[0].replace('?', '');
 
-	// Validate / clean input
+	// Validate / clean input.
 	let titleText = document.getElementById('showcase_title').value.trim();
 
-	// Initialize facet parameters
+	// Initialize facet and query parameters.
 	let qInclude = 'qInclude=';
 	let qExclude = 'qExclude=';
+	let mFacet = 'multiFacets=';
+	let query = 'q=';
 
-	// Loop through Primo URL parameters and add parameters to PNX search URL
+	// Loop through Primo URL parameters and construct API URL.
 	for(let index in searchParams) {
 		let components = searchParams[index].split('=');
 		let param = components[0];
 		let value = components[1];
 		if(param == 'query') {
-			param = 'q';
+			query += value + ';';
+			param = value = '';
 		}
 		else if(param == 'search_scope') {
 			param = 'scope';
 		}
-		else if(param == 'facet') {
-			let valueParts = value.match(/^(.*),(.*),(.*)$/);
+		else if(param == 'facet' || param == 'mfacet') {
+			let valueParts = value.match(/^(.*?),(.*?),(.*?),?1?$/);
 			let facetParam = valueParts[1];
 			let facetType = valueParts[2];
-			let facetValue = valueParts[3];
-			if(facetType == 'include') {
-				qInclude += 'facet_' + facetParam + ',exact,' + facetValue + '%7C,%7C';
+			let facetValue = valueParts[3].replace(',1', '');
+			if(param == 'facet') {
+				if(facetType == 'include') {
+					qInclude += 'facet_' + facetParam + ',exact,' + facetValue + '%7C,%7C';
+				}
+				else {
+					qExclude += 'facet_' + facetParam + ',exact,' + facetValue + '%7C,%7C';
+				}
+				param = value = '';
 			}
-			else {
-				qExclude += 'facet_' + facetParam + ',exact,' + facetValue + '%7C,%7C';
+			else { // param == 'mfacet'
+				mFacet += 'facet_' + facetParam + ',' + facetType + ',' + facetValue + '%7C,%7C';
+				param = value = '';
 			}
-			param = value = '';
 		}
-		primoPNXSUrl += param + '=' + value + '&';
+		else if(param == 'sortby') {
+			param = 'sort';
+		}
+		primoAPIUrl += param + '=' + value + '&';
 	}
 
-	// Trim last '%7C,%7C' off facet parameters and append to PNX search URL
-	primoPNXSUrl += qInclude.replace(/%7C,%7C$/, '') + '&' + qExclude.replace(/%7C,%7C$/, '');
-	console.log(primoPNXSUrl);
+	// Trim last '%7C,%7C' off facet parameters, last ';' off query paramter and append to PNX search URL
+	primoAPIUrl += qInclude.replace(/%7C,%7C$/, '') + '&' +
+		qExclude.replace(/%7C,%7C$/, '') + '&' +
+		mFacet.replace(/%7C,%7C$/, '') + '&' +
+		query.replace(/;$/, '');
 
 	// Add showcase element to showcase div and textarea and highlight text in textarea for copying
 	let showcase = '<search-carousel ' +
 		'titleText="' + titleText + '" ' +
 		'titleLink="' + primoSearchUrl.href + '" ' +
-		'searchUrl="' + primoPNXSUrl + '">' +
+		'searchUrl="' + primoAPIUrl + '">' +
 		'</search-carousel>';
 	document.getElementById('showcase').innerHTML = showcase;
 	document.getElementById('showcase_code').innerHTML = showcase;
-	//document.getElementById('showcase_code').select();
 
 }
 
 function copyShowcaseCode() {
+
 	let showcaseCode = document.getElementById('showcase_code');
 	showcaseCode.select();
+	// Note: The following won't work in an http environment; needs to be https.
 	navigator.clipboard.writeText(showcaseCode.value);
+
 }
